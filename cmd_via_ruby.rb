8f7afaea3c6dc078f874cef494b2a5fa2aa2a144
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-08 18:07:15 +0800

    show pwd return value

--- cmd_via_ruby.rb
+++ cmd_via_ruby.rb
@@ -1,7 +1,15 @@
 # cmd_via_ruby.rb
 
-dir_name = "tt3"
+dir_name = "tt5"
 # `pwd`
 # p "mkdir #{dir_name}"
-# `mkdir #{dir_name}`
-exec("cd #{dir_name}")
+`mkdir #{dir_name}`
+`mkdir tt6`
+# exec("cd #{dir_name}")
+p `pwd`
+
+
+# value = `ls -al`
+# value = `#{cmd}`
+# p value.class
+# p value
