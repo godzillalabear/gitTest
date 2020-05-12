6a9889c448fd08cc0e85b3e7c1beda62031130db
Yushan Cheng <b10432020@gapps.ntust.edu.tw>
2020-05-12 15:31:58 +0800

    get first layer files and directories

--- ruby_file.rb
+++ ruby_file.rb
@@ -1,5 +1,22 @@
 # ruby_file.rb
 
-@path = "/Users/godzillalabear/Documents/Astro_Camp/gitServer"
+user_name = "merry"
+repo_title = "aabbcc"
 
-p @path
+path = "/Users/godzillalabear/Documents/Astro_Camp/gitServer/#{user_name}/#{repo_title}"
+
+
+@files = []
+@dirs = []
+
+Dir.entries(path).each do |file|
+  if File.file?(path+"/"+file)
+    @files << file
+  else
+    @dirs << file
+  end
+end
+
+
+p @files
+p @dirs
